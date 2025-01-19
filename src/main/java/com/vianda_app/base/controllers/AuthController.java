326731a0b06a81e4fc90f45fee6587b38d8b7fd8
agus <agustinolivieri30@gmail.com>
2025-01-19 16:46:47 -0300

    cleaning imports

--- src/main/java/com/vianda_app/base/controllers/AuthController.java
+++ src/main/java/com/vianda_app/base/controllers/AuthController.java
@@ -2,14 +2,7 @@ package com.vianda_app.base.controllers;
 
 import com.vianda_app.base.dtos.LoginRequest;
 import com.vianda_app.base.dtos.RegistroRequest;
-import com.vianda_app.base.entities.Rol;
-import com.vianda_app.base.entities.Token;
-import com.vianda_app.base.entities.Usuario;
-import com.vianda_app.base.repositories.RolRepository;
-import com.vianda_app.base.repositories.TokenRepository;
 import com.vianda_app.base.services.AuthService;
-import com.vianda_app.base.services.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
