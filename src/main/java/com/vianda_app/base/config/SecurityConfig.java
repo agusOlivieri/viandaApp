d8f35d5d0b24003ea37561ddd94440ffb98d201b
agus <agustinolivieri30@gmail.com>
2025-02-21 12:49:24 -0300

    cors from app.prop

--- src/main/java/com/vianda_app/base/config/SecurityConfig.java
+++ src/main/java/com/vianda_app/base/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(req -> req
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/pedidos/**").permitAll()
