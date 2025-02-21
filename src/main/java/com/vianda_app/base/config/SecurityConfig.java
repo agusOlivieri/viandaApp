9448062583e1e2884091d28715200ddae0a06bf5
agus <agustinolivieri30@gmail.com>
2025-02-21 12:35:38 -0300

    trying

--- src/main/java/com/vianda_app/base/config/SecurityConfig.java
+++ src/main/java/com/vianda_app/base/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOriginPattern("*");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
 
